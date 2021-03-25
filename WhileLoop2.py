a555c5d2411f73bb882c3c9948f2e138295411f0
sagsahoo <sagarika.sahoo@oracle.com>
2021-03-25 23:34:39 +0530

    commit

---
+++ WhileLoop2.py
@@ -0,0 +1,15 @@
+i=2
+while(i<=2):
+    print("hello python")
+    i=i+2
+else:
+    print("bye python")
+
+
+    j=0
+    while(j<=3):
+        print(j)
+        j=j+2
+    else:
+        print("do not print J")
+
